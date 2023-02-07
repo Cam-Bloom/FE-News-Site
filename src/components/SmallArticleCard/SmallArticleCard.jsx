931986974573f2e1d2fecc2f53edb5b875f4a76e
Cam-Bloom <cam.w.bloomfield@gmail.com>
2023-02-07 16:45:57 +0000

    Home styling changes

--- src/components/SmallArticleCard/SmallArticleCard.jsx
+++ src/components/SmallArticleCard/SmallArticleCard.jsx
@@ -1,30 +1,32 @@
-import "./SmallArticleCard.css"
-import {  useNavigate } from 'react-router-dom';
+import { FaArrowCircleRight } from "react-icons/fa";
 
+import "./SmallArticleCard.css";
+import { useNavigate } from "react-router-dom";
 
-const SmallArtCard = ({article}) => {
+const SmallArtCard = ({ article }) => {
+	const { title, topic, author, body, created_at, votes, article_img_url, article_id } = article;
+	const navigate = useNavigate();
 
-  const {title, topic, author, body, created_at, votes, article_img_url, article_id} = article
-  const navigate = useNavigate();
-  
-  const readArticle = () => {
-    navigate(`/article/${article_id}`);
-  }
-  
-  return (
-    <li className="smallCard">
-      <img className="smallImg" src={article_img_url} alt={`${title} by ${author}`} />
+	const readArticle = () => {
+		navigate(`/article/${article_id}`);
+	};
 
-      <div className="info">
-        <h3 className="truncate">{title}</h3>
-        <div className="subHeadigns">
-          <h5>By: {author}</h5>
-          <h5>{topic}</h5>
-          <button onClick={readArticle} >Read</button>
-        </div>
-      </div>
-    </li>
-  )
-}
+	return (
+		<li className="smallCard">
+			<img className="smallImg" src={article_img_url} alt={`${title} by ${author}`} />
+			<div className="smallCardInfo">
+				<h3 className="truncate smallCardTitle">{title}</h3>
+				<div className="subHeadigns">
+					<h5>By: {author}</h5>
+					<h5>{topic}</h5>
+				</div>
+				<div className="readMore" onClick={readArticle}>
+					<p>Read more</p>
+					<FaArrowCircleRight className="readArrow" />
+				</div>
+			</div>
+		</li>
+	);
+};
 
-export default SmallArtCard
+export default SmallArtCard;
