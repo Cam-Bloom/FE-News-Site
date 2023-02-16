714df0a185697cac5adb5e54b109b985eb7ef6d1
Cam-Bloom <cam.w.bloomfield@gmail.com>
2023-02-16 08:21:10 +0000

    Custom navigation arrows added to swiper

--- src/components/HomeTopArticle/HomeTopArticle.jsx
+++ src/components/HomeTopArticle/HomeTopArticle.jsx
@@ -1,41 +1,53 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 import { fetchArticles } from "../../utils";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper";
 import HomeTopArticleCard from "../HomeTopArticleCard/HomeTopArticleCard";
-import 'swiper/css/autoplay';
-
-
+import "swiper/css/autoplay";
+import "./HomeTopArticle.css";
 
 const HomeTopArticle = () => {
-    const [topArticlesFromApi, setTopArticlesFromApi] = useState([]);
-  
-    useEffect(() => {
+	const [topArticlesFromApi, setTopArticlesFromApi] = useState([]);
+	const swiperNavPrevRef = useRef(null);
+	const swiperNavNextRef = useRef(null);
+
+	useEffect(() => {
 		// setLoading(true);
-		fetchArticles({limit:5, sort_by:'votes'}).then((data) => {
+		fetchArticles({ limit: 5, sort_by: "votes" }).then((data) => {
 			setTopArticlesFromApi(data.articles);
 			// setLoading(false);
 		});
 	}, []);
-  
-    
-  
-    return (
-    <div>
-        {/* <h2>Top Articles</h2> */}
-        <Swiper
-          autoplay={{delay:4000,pauseOnMouseEnter:true}}
-          modules={[Autoplay]}>
-            {topArticlesFromApi.map((article) => {
-                return (
-                    <SwiperSlide key={article.article_id}>
-                        <HomeTopArticleCard key={article.article_id} article={article}/>
-                    </SwiperSlide>
-                )
-            })}
-        </Swiper>
-    </div>
-  )
-}
 
-export default HomeTopArticle
+	return (
+		<div className="TopArticleSection">
+			{/* <h2>Top Articles</h2> */}
+			<Swiper
+				autoplay={{ delay: 4000, pauseOnMouseEnter: true }}
+				navigation={{
+					prevEl: swiperNavPrevRef.current,
+					nextEl: swiperNavNextRef.current,
+				}}
+                loop
+				modules={[Autoplay, Navigation]}
+			>
+				{topArticlesFromApi.map((article) => {
+					return (
+						<SwiperSlide key={article.article_id}>
+							<HomeTopArticleCard key={article.article_id} article={article} />
+						</SwiperSlide>
+					);
+				})}
+				<div className="swiperNavPrev" ref={swiperNavPrevRef}>
+					<FaArrowCircleLeft />
+				</div>
+				<div className="swiperNavNext" ref={swiperNavNextRef}>
+					<FaArrowCircleRight />
+				</div>
+			</Swiper>
+		</div>
+	);
+};
+
+export default HomeTopArticle;
--- src/components/HomeTopArticleCard/HomeTopArticleCard.jsx
+++ src/components/HomeTopArticleCard/HomeTopArticleCard.jsx
@@ -1,14 +1,15 @@
-import { Swiper, SwiperSlide, } from "swiper/react";
 import "./HomeTopArticleCard.css"
+import { useNavigate } from "react-router-dom";
 
 const HomeTopArticleCard = ({article}) => {
 
 	const { title, topic, author, article_img_url, article_id } = article;
+	const navigate = useNavigate();
 
   return (
     <div className="TopArticleCard">
         <img src={article_img_url} className='TopArticleImg'></img>
-        <h2 className="TopArticleCardTitle">{title}</h2>
+        <h2 className="TopArticleCardTitle" onClick={() => {navigate(`/article/${article_id}`)}}>{title} </h2>
     </div>
   )
 }
